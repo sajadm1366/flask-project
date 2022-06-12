c9c9a5ebed530d49827cb3f05ef0da9d10188b08
sajad <mehrizis@gmail.com>
2022-06-12 21:15:51 +0200

    upadted files

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
+from email.policy import default
+import imp
+from re import U
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data_base.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+       task_content = request.form['content']
+       new_task = Tasks(content=task_content)
+       db.session.add(new_task)
+       db.session.commit()
+       return redirect(url_for('home'))
+    else:
+        tasks = Tasks.query.all()
+        return render_template("home.html", tasks = tasks)
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    taks_to_delete = Tasks.query.get_or_404(id) 
+    db.session.delete(taks_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task  = Tasks.query.get_or_404(id)
+    if request.method == 'POST':
+       task.content = request.form['content']
+       db.session.commit()
+       return redirect('/')
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
